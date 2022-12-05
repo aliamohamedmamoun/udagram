7a7f071c885a3028e1ca487466b0ab0d709d1d85
Alia <alia.maamoun@oracle.com>
2022-12-05 11:04:27 +0200

    fix linting issues

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
+  appName: 'Udagram',
   apiHost:
-    "http://udagram-api-dev.eba-rwhbzp3x.us-east-1.elasticbeanstalk.com/api/v0",
+    'http://udagram-api-dev.eba-rwhbzp3x.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
